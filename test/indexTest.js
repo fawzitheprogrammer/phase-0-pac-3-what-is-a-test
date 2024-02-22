164d43b29bc52fb8b672446ed09e5c5a21ebd11c
Fawzi Gharib <fawz11gharib@gmail.com>
2024-02-22 23:49:10 +0300

    Assignment Done

--- test/indexTest.js
+++ test/indexTest.js
@@ -29,10 +29,24 @@ describe('what-is-a-test', () => {
 // The code below ensures that students who are using CodeGrade will get credit 
 // for the code-along in Canvas; you can disregard it.
 
-describe('', () => {
-  describe('', () => {
-    it('', () => {
-      return true;
+describe('what-is-a-test', () => {
+  describe('Name', () => {
+    it('returns "Susan"', () => {
+      expect(name).toEqual('Susan')
+    })
+  })
+
+
+  describe('Height', () => {
+    it('is less than 40', () => {
+      expect(height).toBeLessThan(40)
+    })
+  })
+
+  describe('Message', () => {
+    it('gives the name and height', () => {
+      expect(message).toInclude(name)
+      expect(message).toInclude(height)
     })
   })
 })
